51a6c2102c7651400d5cb61b55a01a8e4b127267
Meena044 <meena290470@gmail.com>
2025-09-16 01:21:24 +0530

    javascript exercise 7 completed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let spiltedString = str.split('').reverse().join('');
+    return spiltedString
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+    // let newArr = [];
+    for(let i=arr.length-1; i>=0; i--){
+        if(arr[i]===remove)
+            arr.splice(i, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// i=0 0<3 arr[0]= "hey", "hey"==="hey"
