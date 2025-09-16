d81113189fdbfc1997c20ebfee05b743953afe19
Meena044 <meena290470@gmail.com>
2025-09-16 23:12:14 +0530

    completed 10 & 11 javascript excercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+      let celsius = ((fahr - 32) * 5) / 9;
+      return Number(celsius.toFixed(1));   
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+      let fahrenheit = ((cels * 9) / 5) + 32;
+      return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
