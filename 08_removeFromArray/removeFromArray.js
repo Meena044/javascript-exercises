b573b409f4bef614d77acedb92312342ddde4f29
Meena044 <meena290470@gmail.com>
2025-09-16 22:21:48 +0530

    completed javascript 8 & 9 excercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,13 +1,15 @@
-const removeFromArray = function(arr, remove) {
-    // let newArr = [];
-    for(let i=arr.length-1; i>=0; i--){
-        if(arr[i]===remove)
-            arr.splice(i, 1);
-    }
-    return arr;
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    arr.forEach(element => {
+        if(!args.includes(element))
+            newArr.push(element);
+    });
+
+    return newArr;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
 
-// i=0 0<3 arr[0]= "hey", "hey"==="hey"
+// i=0 0<3 arr[0]= "hey", "hey"==="hey" 
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if((Number.isInteger(start) && start > 0) && (Number.isInteger(end) && end > 0)){
+        if(start <= end){
+            for(let i=start; i<=end; i++){
+                sum = sum + start;
+                start +=1;
+            }
+        }
+        else if(start >= end){
+            for(let i=start; i>=end; i--){
+                sum = sum + start;
+                start -=1;
+            }
+        }
+    }
+    else {
+        sum = "ERROR"
+    }
+    return sum;
 };
 
 // Do not edit below this line
