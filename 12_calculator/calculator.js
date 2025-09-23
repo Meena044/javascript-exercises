8f66627c560e3fe0cd3a3c8f5cecb71657d299a2
Meena044 <meena290470@gmail.com>
2025-09-23 20:22:54 +0530

    completed calulator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -8,21 +8,31 @@ const subtract = function(a, b) {
    return sum;
 };
 
-const sum = function(...args) {
-  let total= args.reduce((acc, arg) => acc + arg, 0); 
+const sum = function([...args]) {
+  let total= args.reduce((acc, item) => acc + item, 0); 
   return total;
 };
 
-const multiply = function() {
-
+const multiply = function([...values]) {
+    let multiSum = values.reduce((acc,item)=> acc * item, 1 );
+    return multiSum;
 };
 
-const power = function() {
+const power = function(val, power) {
+  // let powSum = Math.pow(val,power); // two methods to write
+  let powSum = val ** power;
+  return powSum;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factTotal=1;
+  for(let i = 1; i <=num; i++){
+    factTotal = i * factTotal; 
+    
+  }
+  return factTotal;
+
 };
 
 // Do not edit below this line
